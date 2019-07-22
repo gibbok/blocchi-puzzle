2af0f720572f9add55b8062a1c8eb9ba7c364fc8
GibboK <gibbok.coding@gmail.com>
2019-07-22 22:31:50 +0200

    Use eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom';
 
 interface Props {
-   name: string
+    name: string;
 }
 
 class App extends React.Component<Props> {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
 }
 
-var mountNode = document.getElementById("app");
+var mountNode = document.getElementById('app');
 ReactDOM.render(<App name="Jane" />, mountNode);
