90c90a2185f5e45aaec16a58fda5820a591aa397
GibboK <gibbok.coding@gmail.com>
2019-07-22 23:33:47 +0200

    save

--- src/hello.test.ts
+++ src/hello.test.ts
@@ -1,1 +1,2 @@
-test('adds 1 + 2 to equal 3', (): number => expect(1 + 2).toBe(3));
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+test('adds 1 + 2 to equal 3', () => expect(1 + 2).toBe(3));
