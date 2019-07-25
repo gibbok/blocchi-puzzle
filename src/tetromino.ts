45d2472dc0de59e2cd1cb963b734cbdaed4012c7
GibboK <gibbok.coding@gmail.com>
2019-07-25 21:36:41 +0200

    Refactory type for teros

--- src/tetromino.ts
+++ src/tetromino.ts
@@ -1,144 +1,151 @@
 import { TetrominoEnum, Tetromino, Orientation } from './types';
 
+const Z = TetrominoEnum.Z;
+const S = TetrominoEnum.S;
+const J = TetrominoEnum.J;
+const T = TetrominoEnum.T;
+const I = TetrominoEnum.I;
+const O = TetrominoEnum.O;
+
 /* eslint-disable prettier/prettier */
 const pieces: Record<TetrominoEnum, readonly Tetromino[]> = {
   Z: [
     [
-      [1, 1, 0],
-      [0, 1, 1],
+      [Z, Z, 0],
+      [0, Z, Z],
       [0, 0, 0]
     ],
     [
-      [0, 0, 1],
-      [0, 1, 1],
-      [0, 1, 0]
+      [0, 0, Z],
+      [0, Z, Z],
+      [0, Z, 0]
     ],
     [
       [0, 0, 0],
-      [1, 1, 0],
-      [0, 1, 1]
+      [Z, Z, 0],
+      [0, Z, Z]
     ],
     [
-      [0, 1, 0],
-      [1, 1, 0],
-      [1, 0, 0]
+      [0, Z, 0],
+      [Z, Z, 0],
+      [Z, 0, 0]
     ]
   ],
   S: [
     [
-      [0, 1, 1],
-      [1, 1, 0],
+      [0, S, S],
+      [S, S, 0],
       [0, 0, 0]
     ],
     [
-      [0, 1, 0],
-      [0, 1, 1],
-      [0, 0, 1]
+      [0, S, 0],
+      [0, S, S],
+      [0, 0, S]
     ],
     [
       [0, 0, 0],
-      [0, 1, 1],
-      [1, 1, 0]
+      [0, S, S],
+      [S, S, 0]
     ],
     [
-      [1, 0, 0],
-      [1, 1, 0],
-      [0, 1, 0]
+      [S, 0, 0],
+      [S, S, 0],
+      [0, S, 0]
     ]
   ],
   J: [
     [
-      [0, 1, 0],
-      [0, 1, 0],
-      [1, 1, 0]
+      [0, J, 0],
+      [0, J, 0],
+      [J, J, 0]
     ],
     [
-      [1, 0, 0],
-      [1, 1, 1],
+      [J, 0, 0],
+      [J, J, J],
       [0, 0, 0]
     ],
     [
-      [0, 1, 1],
-      [0, 1, 0],
-      [0, 1, 0]
+      [0, J, J],
+      [0, J, 0],
+      [0, J, 0]
     ],
     [
       [0, 0, 0],
-      [1, 1, 1],
-      [0, 0, 1]
+      [J, J, J],
+      [0, 0, J]
     ]
   ],
   T: [
     [
       [0, 0, 0],
-      [1, 1, 1],
-      [0, 1, 0]
+      [T, T, T],
+      [0, T, 0]
     ],
     [
-      [0, 1, 0],
-      [1, 1, 0],
-      [0, 1, 0]
+      [0, T, 0],
+      [T, T, 0],
+      [0, T, 0]
     ],
     [
-      [0, 1, 0],
-      [1, 1, 1],
+      [0, T, 0],
+      [T, T, T],
       [0, 0, 0]
     ],
     [
-      [0, 1, 0],
-      [0, 1, 1],
-      [0, 1, 0]
+      [0, T, 0],
+      [0, T, T],
+      [0, T, 0]
     ],
   ],
   I: [
     [
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
-      [0, 1, 0, 0],
+      [0, I, 0, 0],
+      [0, I, 0, 0],
+      [0, I, 0, 0],
+      [0, I, 0, 0],
     ],
     [
       [0, 0, 0, 0],
-      [1, 1, 1, 1],
+      [I, I, I, I],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
     ],
     [
-      [0, 0, 1, 0],
-      [0, 0, 1, 0],
-      [0, 0, 1, 0],
-      [0, 0, 1, 0],
+      [0, 0, I, 0],
+      [0, 0, I, 0],
+      [0, 0, I, 0],
+      [0, 0, I, 0],
     ],
     [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
-      [1, 1, 1, 1],
+      [I, I, I, I],
       [0, 0, 0, 0],
     ],
   ],
   O: [
     [
       [0, 0, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
+      [0, O, O, 0],
+      [0, O, O, 0],
       [0, 0, 0, 0],
     ],
     [
       [0, 0, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
+      [0, O, O, 0],
+      [0, O, O, 0],
       [0, 0, 0, 0],
     ],
     [
       [0, 0, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
+      [0, O, O, 0],
+      [0, O, O, 0],
       [0, 0, 0, 0],
     ],
     [
       [0, 0, 0, 0],
-      [0, 1, 1, 0],
-      [0, 1, 1, 0],
+      [0, O, O, 0],
+      [0, O, O, 0],
       [0, 0, 0, 0],
     ],
   ],
@@ -147,9 +154,9 @@ const pieces: Record<TetrominoEnum, readonly Tetromino[]> = {
 
 const factoryTetro = (t: TetrominoEnum): ((o: Orientation) => Tetromino) => (o: Orientation): Tetromino => pieces[t][o];
 
-const mkTetroZ = factoryTetro(TetrominoEnum.Z);
-const mkTetroS = factoryTetro(TetrominoEnum.S);
-const mkTetroJ = factoryTetro(TetrominoEnum.J);
-const mkTetroT = factoryTetro(TetrominoEnum.T);
-const mkTetroI = factoryTetro(TetrominoEnum.I);
-const mkTetroO = factoryTetro(TetrominoEnum.O);
+// const mkTetroZ = factoryTetro(TetrominoEnum.Z);
+// const mkTetroS = factoryTetro(TetrominoEnum.S);
+// const mkTetroJ = factoryTetro(TetrominoEnum.J);
+// const mkTetroT = factoryTetro(TetrominoEnum.T);
+// const mkTetroI = factoryTetro(TetrominoEnum.I);
+// const mkTetroO = factoryTetro(TetrominoEnum.O);
--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,6 @@ export enum TetrominoEnum {
     O = 'O',
 }
 
-export type Cell = 0 | 1;
+export type Cell = 0 | TetrominoEnum;
 
 export type Tetromino = readonly (readonly Cell[])[];
