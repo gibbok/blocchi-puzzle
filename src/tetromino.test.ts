228a0699c2dda2416284c4c0c334b4b127619e31
GibboK <gibbok.coding@gmail.com>
2019-07-25 23:14:31 +0200

    save

--- src/tetromino.test.ts
+++ src/tetromino.test.ts
@@ -1,8 +1,8 @@
-import { TetroEnum, Orientation } from "./types";
 import { x } from "./tetromino";
 
 describe('tetromino', () => {
   describe('factoryTetro', () => {
+    test('x', ()=> expect(1).toEqual(1))
     test('x',()=> expect(x()).toEqual(10))
   })
 })
