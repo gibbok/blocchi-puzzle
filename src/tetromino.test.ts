5a07bb1895d985ed5f07a54f72e2e56b08e73f8a
GibboK <gibbok.coding@gmail.com>
2019-07-26 08:49:46 +0200

    add tests

--- src/tetromino.test.ts
+++ src/tetromino.test.ts
@@ -4,9 +4,60 @@ import { Z, Orientation } from './types';
 describe('tetromino', () => {
   describe('factoryTetro', () => {
     it('should return tetro: position: 0/0, type: Z, orientation: N', () =>
-      expect(factoryTetro({ x: 0, y: 0 })(Z)(Orientation.N)).toEqual({
-        position: { x: 0, y: 0 },
-        tetromino: [['Z', 'Z', 0], [0, 'Z', 'Z'], [0, 0, 0]]
+      expect(
+        factoryTetro({
+          x: 0,
+          y: 0
+        })(Z)(Orientation.N)
+      ).toEqual({
+        position: {
+          x: 0,
+          y: 0
+        },
+        // eslint-disable-next-line prettier/prettier
+        tetromino: [
+          [Z, Z, 0],
+          [0, Z, Z],
+          [0, 0, 0]
+        ]
+      }));
+
+    it('should return tetro: position: 5/5, type: Z, orientation: N', () =>
+      expect(
+        factoryTetro({
+          x: 5,
+          y: 5
+        })(Z)(Orientation.N)
+      ).toEqual({
+        position: {
+          x: 5,
+          y: 5
+        },
+        // eslint-disable-next-line prettier/prettier
+        tetromino: [
+          [Z, Z, 0],
+          [0, Z, Z],
+          [0, 0, 0]
+        ]
+      }));
+
+    it('should return tetro: position: 5/5, type: Z, orientation: E', () =>
+      expect(
+        factoryTetro({
+          x: 0,
+          y: 0
+        })(Z)(Orientation.E)
+      ).toEqual({
+        position: {
+          x: 0,
+          y: 0
+        },
+        // eslint-disable-next-line prettier/prettier
+        tetromino: [
+          [0, 0, Z],
+          [0, Z, Z],
+          [0, Z, 0]
+        ]
       }));
   });
 });
