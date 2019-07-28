edc1717400414724b81afca727c7c8e32ba1c3d7
GibboK <gibbok.coding@gmail.com>
2019-07-28 14:15:59 +0200

    save

--- src/data.support.test.ts
+++ src/data.support.test.ts
@@ -1,8 +1,6 @@
-import { TetroEnum, Tetro, DirectionEnum, Z, S, J, T, I, O, TetroPieces } from './types';
-import { randomInt } from 'fp-ts/lib/Random';
-import { IO, io } from 'fp-ts/lib/IO';
+import { TetroPieces, Z, S, J, T, I, O } from './types';
 
-const pieces: TetroPieces = {
+export const testPieces: TetroPieces = {
   Z: {
     N: [[Z, Z, 0], [0, Z, Z], [0, 0, 0]],
     E: [[0, 0, Z], [0, Z, Z], [0, Z, 0]],
@@ -40,17 +38,3 @@ const pieces: TetroPieces = {
     W: [[0, 0, 0, 0], [0, O, O, 0], [0, O, O, 0], [0, 0, 0, 0]]
   }
 };
-
-export const factoryTetro = (t: TetroEnum) => (o: DirectionEnum): Tetro => pieces[t][o];
-
-export const getRandomTetro = (): IO<Tetro> => {
-  const rndInt = randomInt(0, Object.keys(TetroEnum).length - 1)();
-  const rndEnum = Object.keys(TetroEnum)[rndInt];
-  return io.of(pieces[rndEnum as TetroEnum][DirectionEnum.N]);
-};
-// const x = factoryTetro({ x: 0, y: 0 })(Z)(Orientation.N);
-// const mkTetroS = factoryTetro(TetrominoEnum.S);
-// const mkTetroJ = factoryTetro(TetrominoEnum.J);
-// const mkTetroT = factoryTetro(TetrominoEnum.T);
-// const mkTetroI = factoryTetro(TetrominoEnum.I);
-// const mkTetroO = factoryTetro(TetrominoEnum.O);
--- src/tetromino.test.ts
+++ src/tetromino.test.ts
@@ -1,173 +1,7 @@
 import { factoryTetro, getRandomTetro } from './tetromino';
-import { Z, S, J, T, O, I, TetroEnum, DirectionEnum, TetroPieces } from './types';
+import { TetroEnum, DirectionEnum } from './types';
 import { stub } from 'sinon';
-
-const dataTetro: TetroPieces = {
-  Z: {
-    // eslint-disable-next-line prettier/prettier
-    N: [
-      [Z, Z, 0],
-      [0, Z, Z],
-      [0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E: [
-      [0, 0, Z],
-      [0, Z, Z],
-      [0, Z, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S: [
-      [0, 0, 0],
-      [Z, Z, 0],
-      [0, Z, Z],
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W: [
-      [0, Z, 0],
-      [Z, Z, 0],
-      [Z, 0, 0]
-    ]
-  },
-  S: {
-    // eslint-disable-next-line prettier/prettier
-    N: [
-      [0, S, S],
-      [S, S, 0],
-      [0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E: [
-      [0, S, 0],
-      [0, S, S],
-      [0, 0, S]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S: [
-      [0, 0, 0],
-      [0, S, S],
-      [S, S, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W: [
-      [S, 0, 0],
-      [S, S, 0],
-      [0, S, 0]
-    ],
-  },
-  J: {
-    // eslint-disable-next-line prettier/prettier
-    N:[
-      [0, J, 0],
-      [0, J, 0],
-      [J, J, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E:[
-      [J, 0, 0],
-      [J, J, J],
-      [0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S:[
-      [0, J, J],
-      [0, J, 0],
-      [0, J, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W:[
-      [0, 0, 0],
-      [J, J, J],
-      [0, 0, J]
-    ]
-  },
-  T: {
-    // eslint-disable-next-line prettier/prettier
-    N:[
-      [0, 0, 0],
-      [T, T, T],
-      [0, T, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E:[
-      [0, T, 0],
-      [T, T, 0],
-      [0, T, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S:[
-      [0, T, 0],
-      [T, T, T],
-      [0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W:[
-      [0, T, 0],
-      [0, T, T],
-      [0, T, 0]
-    ]
-  },
-  I: {
-    // eslint-disable-next-line prettier/prettier
-    N:[
-      [0, I, 0, 0],
-      [0, I, 0, 0],
-      [0, I, 0, 0],
-      [0, I, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E:[
-      [0, 0, 0, 0],
-      [I, I, I, I],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S:[
-      [0, 0, I, 0],
-      [0, 0, I, 0],
-      [0, 0, I, 0],
-      [0, 0, I, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W:[
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [I, I, I, I],
-      [0, 0, 0, 0]
-    ],
-  },
-  O: {
-    // eslint-disable-next-line prettier/prettier
-    N:[
-      [0, 0, 0, 0],
-      [0, O, O, 0],
-      [0, O, O, 0],
-      [0, 0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    E:[
-      [0, 0, 0, 0],
-      [0, O, O, 0],
-      [0, O, O, 0],
-      [0, 0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    S:[
-      [0, 0, 0, 0],
-      [0, O, O, 0],
-      [0, O, O, 0],
-      [0, 0, 0, 0]
-    ],
-    // eslint-disable-next-line prettier/prettier
-    W:[
-      [0, 0, 0, 0],
-      [0, O, O, 0],
-      [0, O, O, 0],
-      [0, 0, 0, 0]
-    ]
-  }
-};
+import { testPieces } from './data.support.test';
 
 describe('tetromino', () => {
   describe('factoryTetro', () => {
@@ -176,7 +10,7 @@ describe('tetromino', () => {
     tetroKeys.forEach((t: string) =>
       directionKeys.forEach((o: string) => {
         it(`should return tetro of type: ${t}, direction: ${o}`, () => {
-          expect(dataTetro[t as TetroEnum][o as DirectionEnum]).toEqual(
+          expect(testPieces[t as TetroEnum][o as DirectionEnum]).toEqual(
             factoryTetro(t as TetroEnum)(o as DirectionEnum)
           );
         });
@@ -189,7 +23,7 @@ describe('tetromino', () => {
     afterAll(() => stub().restore());
 
     it('should return random tetro', () => {
-      expect(getRandomTetro()()).toEqual(dataTetro[TetroEnum.Z][DirectionEnum.N]);
+      expect(getRandomTetro()()).toEqual(testPieces[TetroEnum.Z][DirectionEnum.N]);
     });
   });
 });
