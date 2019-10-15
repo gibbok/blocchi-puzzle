f7222915188f12625f6d9a09f2caf5531e1e6386
GibboK <gibbok.coding@gmail.com>
2019-10-15 09:15:14 +0200

    save work on reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,6 +1,6 @@
-import { InternalState, Action, ActionEnum, TetroEnum, DirectionEnum } from './types';
+import { InternalState, ActionEnum, TetroEnum, DirectionEnum } from './types';
 import { Store } from 'redux';
-import { configureStore } from 'redux-starter-kit';
+import { configureStore, createReducer } from 'redux-starter-kit';
 
 import {
   mkEmptyBoard,
@@ -37,10 +37,113 @@ export const mkInitialState = () => ({
   isPlay: true // TODO pause before start game
 });
 
-export const reducer = (
-  prevState: InternalState = mkInitialState(),
-  action: Action
-): InternalState => {
+// export const reducer = (
+//   prevState: InternalState = mkInitialState(),
+//   action: Action
+// ): InternalState => {
+//   const {
+//     board,
+//     score,
+//     level,
+//     lines,
+//     currentTetro: { type, direction, x, y },
+//     nextTetro,
+//     isPlay
+//   } = prevState;
+//   switch (action.type) {
+//     case ActionEnum.MoveDown:
+//       const newY = y + 1;
+//       const isOccupiedDown = occupied(type)(direction)(x)(newY)(board);
+//       const foundPosY = recFindAvailablePosY(type)(direction)(x)(newY)(board)(1);
+//       return {
+//         board: isOccupiedDown ? addTetroToBoard(type)(direction)(x)(foundPosY)(board) : board,
+//         score,
+//         level,
+//         lines,
+//         currentTetro: {
+//           type,
+//           direction,
+//           x,
+//           y: isOccupiedDown ? foundPosY : newY
+//         },
+//         nextTetro,
+//         isPlay
+//       };
+//     case ActionEnum.MoveRight:
+//       const newRightX = x + 1;
+//       const isOccupiedRight = occupied(type)(direction)(newRightX)(y)(board);
+//       const foundRightPosX = recFindAvailablePosX(type)(direction)(newRightX)(y)(board)(1);
+//       return {
+//         board: isOccupiedRight ? addTetroToBoard(type)(direction)(foundRightPosX)(y)(board) : board,
+//         score,
+//         level,
+//         lines,
+//         currentTetro: {
+//           type,
+//           direction,
+//           x: isOccupiedRight ? foundRightPosX : newRightX,
+//           y
+//         },
+//         nextTetro,
+//         isPlay
+//       };
+//     case ActionEnum.MoveLeft:
+//       const newLeftX = x - 1;
+//       const isOccupiedLeft = occupied(type)(direction)(newLeftX)(y)(board);
+//       const foundLeftPosX = recFindAvailablePosX(type)(direction)(newLeftX)(y)(board)(-1);
+//       return {
+//         board: isOccupiedLeft ? addTetroToBoard(type)(direction)(foundLeftPosX)(y)(board) : board,
+//         score,
+//         level,
+//         lines,
+//         currentTetro: {
+//           type,
+//           direction,
+//           x: isOccupiedLeft ? foundLeftPosX : newLeftX,
+//           y
+//         },
+//         nextTetro,
+//         isPlay
+//       };
+//     case ActionEnum.MoveUp:
+//       const directionNew = rotateTetroDirectionACW(direction);
+//       const isOccupiedUp = occupied(type)(directionNew)(x)(y)(board);
+//       return {
+//         board,
+//         score,
+//         level,
+//         lines,
+//         currentTetro: {
+//           type,
+//           direction: isOccupiedUp ? direction : directionNew,
+//           x,
+//           y
+//         },
+//         nextTetro,
+//         isPlay
+//       };
+//     case ActionEnum.CheckBoard:
+//       const totRowCompleted = getCompleteRowIdxs(board);
+//       const totRowCompletedLen = totRowCompleted.length;
+//       const newScore = pipe(
+//         totRowCompletedLen,
+//         calcScore
+//       );
+//       return {
+//         board: detectAndRemoveCompletedRows(board),
+//         score: newScore,
+//         level: calcLevel(newScore),
+//         lines: lines + totRowCompletedLen,
+//         currentTetro: { type, direction, x, y },
+//         nextTetro,
+//         isPlay
+//       };
+//     default:
+//       return prevState;
+//   }
+// };
+
+const logicMoveDown = (prevState: InternalState) => {
   const {
     board,
     score,
@@ -50,97 +153,142 @@ export const reducer = (
     nextTetro,
     isPlay
   } = prevState;
-  switch (action.type) {
-    case ActionEnum.MoveDown:
-      const newY = y + 1;
-      const isOccupiedDown = occupied(type)(direction)(x)(newY)(board);
-      const foundPosY = recFindAvailablePosY(type)(direction)(x)(newY)(board)(1);
-      return {
-        board: isOccupiedDown ? addTetroToBoard(type)(direction)(x)(foundPosY)(board) : board,
-        score,
-        level,
-        lines,
-        currentTetro: {
-          type,
-          direction,
-          x,
-          y: isOccupiedDown ? foundPosY : newY
-        },
-        nextTetro,
-        isPlay
-      };
-    case ActionEnum.MoveRight:
-      const newRightX = x + 1;
-      const isOccupiedRight = occupied(type)(direction)(newRightX)(y)(board);
-      const foundRightPosX = recFindAvailablePosX(type)(direction)(newRightX)(y)(board)(1);
-      return {
-        board: isOccupiedRight ? addTetroToBoard(type)(direction)(foundRightPosX)(y)(board) : board,
-        score,
-        level,
-        lines,
-        currentTetro: {
-          type,
-          direction,
-          x: isOccupiedRight ? foundRightPosX : newRightX,
-          y
-        },
-        nextTetro,
-        isPlay
-      };
-    case ActionEnum.MoveLeft:
-      const newLeftX = x - 1;
-      const isOccupiedLeft = occupied(type)(direction)(newLeftX)(y)(board);
-      const foundLeftPosX = recFindAvailablePosX(type)(direction)(newLeftX)(y)(board)(-1);
-      return {
-        board: isOccupiedLeft ? addTetroToBoard(type)(direction)(foundLeftPosX)(y)(board) : board,
-        score,
-        level,
-        lines,
-        currentTetro: {
-          type,
-          direction,
-          x: isOccupiedLeft ? foundLeftPosX : newLeftX,
-          y
-        },
-        nextTetro,
-        isPlay
-      };
-    case ActionEnum.MoveUp:
-      const directionNew = rotateTetroDirectionACW(direction);
-      const isOccupiedUp = occupied(type)(directionNew)(x)(y)(board);
-      return {
-        board,
-        score,
-        level,
-        lines,
-        currentTetro: {
-          type,
-          direction: isOccupiedUp ? direction : directionNew,
-          x,
-          y
-        },
-        nextTetro,
-        isPlay
-      };
-    case ActionEnum.CheckBoard:
-      const totRowCompleted = getCompleteRowIdxs(board);
-      const totRowCompletedLen = totRowCompleted.length;
-      const newScore = pipe(
-        totRowCompletedLen,
-        calcScore
-      );
-      return {
-        board: detectAndRemoveCompletedRows(board),
-        score: newScore,
-        level: calcLevel(newScore),
-        lines: lines + totRowCompletedLen,
-        currentTetro: { type, direction, x, y },
-        nextTetro,
-        isPlay
-      };
-    default:
-      return prevState;
-  }
+  const newY = y + 1;
+  const isOccupiedDown = occupied(type)(direction)(x)(newY)(board);
+  const foundPosY = recFindAvailablePosY(type)(direction)(x)(newY)(board)(1);
+  return {
+    board: isOccupiedDown ? addTetroToBoard(type)(direction)(x)(foundPosY)(board) : board,
+    score,
+    level,
+    lines,
+    currentTetro: {
+      type,
+      direction,
+      x,
+      y: isOccupiedDown ? foundPosY : newY
+    },
+    nextTetro,
+    isPlay
+  };
 };
 
-const store: Store = configureStore({ reducer });
+const logicMoveRight = (prevState: InternalState) => {
+  const {
+    board,
+    score,
+    level,
+    lines,
+    currentTetro: { type, direction, x, y },
+    nextTetro,
+    isPlay
+  } = prevState;
+  const newRightX = x + 1;
+  const isOccupiedRight = occupied(type)(direction)(newRightX)(y)(board);
+  const foundRightPosX = recFindAvailablePosX(type)(direction)(newRightX)(y)(board)(1);
+  return {
+    board: isOccupiedRight ? addTetroToBoard(type)(direction)(foundRightPosX)(y)(board) : board,
+    score,
+    level,
+    lines,
+    currentTetro: {
+      type,
+      direction,
+      x: isOccupiedRight ? foundRightPosX : newRightX,
+      y
+    },
+    nextTetro,
+    isPlay
+  };
+};
+
+const logicMoveUp = (prevState: InternalState) => {
+  const {
+    board,
+    score,
+    level,
+    lines,
+    currentTetro: { type, direction, x, y },
+    nextTetro,
+    isPlay
+  } = prevState;
+  const directionNew = rotateTetroDirectionACW(direction);
+  const isOccupiedUp = occupied(type)(directionNew)(x)(y)(board);
+  return {
+    board,
+    score,
+    level,
+    lines,
+    currentTetro: {
+      type,
+      direction: isOccupiedUp ? direction : directionNew,
+      x,
+      y
+    },
+    nextTetro,
+    isPlay
+  };
+};
+
+const logicMoveLeft = (prevState: InternalState) => {
+  const {
+    board,
+    score,
+    level,
+    lines,
+    currentTetro: { type, direction, x, y },
+    nextTetro,
+    isPlay
+  } = prevState;
+  const newLeftX = x - 1;
+  const isOccupiedLeft = occupied(type)(direction)(newLeftX)(y)(board);
+  const foundLeftPosX = recFindAvailablePosX(type)(direction)(newLeftX)(y)(board)(-1);
+  return {
+    board: isOccupiedLeft ? addTetroToBoard(type)(direction)(foundLeftPosX)(y)(board) : board,
+    score,
+    level,
+    lines,
+    currentTetro: {
+      type,
+      direction,
+      x: isOccupiedLeft ? foundLeftPosX : newLeftX,
+      y
+    },
+    nextTetro,
+    isPlay
+  };
+};
+
+const logicCheckBoard = (prevState: InternalState) => {
+  const {
+    board,
+    lines,
+    currentTetro: { type, direction, x, y },
+    nextTetro,
+    isPlay
+  } = prevState;
+  const totRowCompleted = getCompleteRowIdxs(board);
+  const totRowCompletedLen = totRowCompleted.length;
+  const newScore = pipe(
+    totRowCompletedLen,
+    calcScore
+  );
+  return {
+    board: detectAndRemoveCompletedRows(board),
+    score: newScore,
+    level: calcLevel(newScore),
+    lines: lines + totRowCompletedLen,
+    currentTetro: { type, direction, x, y },
+    nextTetro,
+    isPlay
+  };
+};
+
+export const reducer = createReducer(mkInitialState(), {
+  [ActionEnum.CheckBoard]: logicCheckBoard,
+  [ActionEnum.MoveUp]: logicMoveUp,
+  [ActionEnum.MoveLeft]: logicMoveLeft,
+  [ActionEnum.MoveRight]: logicMoveRight,
+  [ActionEnum.MoveDown]: logicMoveDown
+});
+
+export const store: Store = configureStore({ reducer });
