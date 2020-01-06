9eb056bee7e50618e140f7a21fa6996c3e4c5141
GibboK <gibbok.coding@gmail.com>
2020-01-06 17:34:38 +0100

    scaffold ui

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { Board as BoardType } from '../game';
 import { Tile } from './Tile';
 import styled from 'styled-components';
-import { BOARD_CELLS, BOARD_ROWS } from '../game/settings';
+import { BOARD_CELLS, BOARD_ROWS, TILE_WIDTH } from '../game/settings';
 
 const StyledBoard = styled.div`
   display: grid;
   background-color: darkgray;
-  width: 20rem;
-  height: 40rem;
+  width: ${TILE_WIDTH * BOARD_CELLS}rem;
+  height: ${TILE_WIDTH * BOARD_ROWS}rem;
   grid-template-rows: repeat(${BOARD_ROWS}, 1fr);
   grid-template-columns: repeat(${BOARD_CELLS}, 1fr);
   align-items: center;
--- src/game/settings.ts
+++ src/game/settings.ts
@@ -2,8 +2,8 @@ export const BOARD_ROWS = 20;
 export const BOARD_CELLS = 10;
 export const BOARD_GAP = 0.5;
 
-export const TILE_WIDTH = 1;
-export const TILE_HEIGHT = 1;
+export const TILE_WIDTH = 2;
+export const TILE_HEIGHT = 2;
 
 export enum TITLE_COLOR_ENUM {
   Z = 'red',
