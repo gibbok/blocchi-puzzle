31f644f1af5ac446e5b97e9942bfbf1b08cc3369
GibboK <gibbok.coding@gmail.com>
2020-04-17 12:03:14 +0200

    save

--- src/components/Next.tsx
+++ src/components/Next.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { TetroDef } from '~game/types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const StyledNext = styled.div`
+  background-color: yellow;
+`;
 
 export const Next = ({ nextTetro }: { nextTetro: TetroDef }) => (
-  <div>{JSON.stringify(nextTetro)}</div>
+  <StyledNext>{JSON.stringify(nextTetro)}</StyledNext>
 );
