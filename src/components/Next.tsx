06f7c5fb8a9768cf9dea35cc6342d24b6f285ec6
GibboK <gibbok.coding@gmail.com>
2020-04-17 13:59:48 +0200

    Render next basic version

--- src/components/Next.tsx
+++ src/components/Next.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TetroDef } from '~game/types';
+import { TetroDef, TetroEnum, NoTetroEnum, NoTetro } from '~game/types';
 import styled from 'styled-components';
 import { TILE_WIDTH } from '~game/settings';
 import { Tile } from './Tile';
@@ -9,7 +9,7 @@ const SIZE_NEXT = 4;
 
 const StyledNext = styled.div`
   display: grid;
-  background-color: yellow;
+  background-color: white;
   width: ${TILE_WIDTH * SIZE_NEXT}rem;
   height: ${TILE_WIDTH * SIZE_NEXT}rem;
   grid-template-rows: repeat(${SIZE_NEXT}, 1fr);
@@ -21,11 +21,19 @@ export const Next = ({ nextTetro }: { nextTetro: TetroDef }) => (
     <StyledNext>
       {new Array(SIZE_NEXT).fill(0).map((rowB, rowIdxB) =>
         new Array(SIZE_NEXT).fill(0).map((_clmB: number, cmlIdxB: number) => {
-          return (
-            <div key={cmlIdxB}>
-              {rowIdxB} / {cmlIdxB}
-            </div>
-          );
+          const tetro = getTetroFromPieces(nextTetro.type, nextTetro.direction);
+          const hasPiece = tetro[rowIdxB] !== undefined && tetro[cmlIdxB] !== undefined;
+          if (hasPiece) {
+            const piece = tetro[rowIdxB][cmlIdxB];
+            const hasPieceInTetro = piece !== NoTetro;
+            if (hasPieceInTetro) {
+              return <Tile key={cmlIdxB} variant={piece} debug={{ y: rowIdxB, x: cmlIdxB }} />;
+            } else {
+              return <div key={cmlIdxB}>{hasPieceInTetro}</div>;
+            }
+          } else {
+            return <div key={cmlIdxB}>xx</div>;
+          }
         })
       )}
       {/* {getTetroFromPieces(nextTetro.type, nextTetro.direction).map((row, rowX) =>
