79e45ead38af938d68a5745e0bd221906bb567f5
GibboK <gibbok.coding@gmail.com>
2020-04-19 15:11:05 +0200

    Add better types to gameloop

--- src/components/GameLoop.tsx
+++ src/components/GameLoop.tsx
@@ -1,26 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { gameSlice } from '~store';
+import { Action } from '@reduxjs/toolkit';
 const {
   actions: { moveDown }
 } = gameSlice;
 
 type Props = Readonly<{}>;
 
-let animId = 0;
+let animId = -1;
 
 export const GameLoop = ({}: Props) => {
   const dispatch = useDispatch();
   const [lastTime, setLastTime] = React.useState(0);
 
-  const loop = (time: number) => {
+  const loop = (time: number): void | Action => {
     const progress = time - lastTime;
     if (progress >= 1000) {
       setLastTime(time);
       return dispatch(moveDown());
     }
     animId = window.requestAnimationFrame(loop);
-    return animId;
+    return undefined;
   };
 
   const cleanAnimation = (id: number) => window.cancelAnimationFrame(id);
