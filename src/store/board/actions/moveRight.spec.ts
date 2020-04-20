72e6dd35fae4763a448d48c022f8962e79330bed
GibboK <gibbok.coding@gmail.com>
2020-04-20 21:56:49 +0200

    fix bug in test

--- src/store/board/actions/moveRight.spec.ts
+++ src/store/board/actions/moveRight.spec.ts
@@ -12,12 +12,13 @@ describe('Move Right', () => {
   it('should increase current tetro x position, leaving the board un touched, no collission', () => {
     const test: InternalState = {
       ...INITIAL_STATE,
-      currentTetro: { ...INITIAL_STATE.currentTetro, x: 1 }
+      currentTetro: { ...INITIAL_STATE.currentTetro, x: 4 }
     };
     const r = reducer(INITIAL_STATE, moveRight);
     expect(r).toEqual(test);
   });
-  it('SPO 1 - should not push current tetro x position over the edge, leaving the board un touched, no collission', () => {
+
+  it('should not push current tetro x position over the edge, leaving the board un touched, no collission', () => {
     const initialState: InternalState = {
       ...INITIAL_STATE,
       currentTetro: { ...INITIAL_STATE.currentTetro, x: 6 }
