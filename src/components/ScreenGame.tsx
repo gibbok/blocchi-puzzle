270297850c80e4ebac2d85d90a24c77dd7e2e67a
GibboK <gibbok.coding@gmail.com>
2020-05-07 13:12:07 +0200

    adjust layout next

--- src/components/ScreenGame.tsx
+++ src/components/ScreenGame.tsx
@@ -4,19 +4,27 @@ import { Keyboard } from './Keyboard';
 import { BoardContainer, NextContainer, InfoContainer } from '~containers';
 import { GameLoopContainer } from '~containers/GameLoopContainer';
 
+
 const ScreenGameStyled = styled.div`
   display: flex;
   justify-content: center;
 `;
 
+const Status = styled.div`
+  margin-top: 4rem;
+  margin-left: 4rem;
+`;
+
 export const ScreenGame = ({}: {}) => (
-  <ScreenGameStyled>
+  <>
     <GameLoopContainer />
     <Keyboard />
-    <BoardContainer />
-    <div>
-      <NextContainer />
-      <InfoContainer />
-    </div>
-  </ScreenGameStyled>
+    <ScreenGameStyled>
+      <BoardContainer />
+      <Status>
+        <NextContainer />
+        <InfoContainer />
+      </Status>
+    </ScreenGameStyled>
+  </>
 );
