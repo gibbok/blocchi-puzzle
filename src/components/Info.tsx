c9c78a8fa3b8e391d313ebefe9ceecd9ed8f40de
GibboK <gibbok.coding@gmail.com>
2020-05-07 17:50:17 +0200

    scaffolding divider

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -13,12 +13,40 @@ const Group = styled.div`
   color: #442217;
   opacity: 0.8;
   text-shadow: 0px 1px 0px rgba(255, 255, 255, 0.4), 0px -1px 0px rgba(0, 0, 0, 0.8);
-  margin-bottom: 4rem;
+  padding: 3rem 0 3rem 0;
 `;
 
 const Line = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const Divider = styled.div`
+  position: relative;
+  height: 1px;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 5%;
+    right: 5%;
+    width: 90%;
+    height: 1px;
+    background-image: linear-gradient(to right, transparent, rgb(48, 49, 51), transparent);
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    top: -7px;
+    left: calc(50% - 7px);
+    width: 14px;
+    height: 14px;
+    transform: rotate(45deg);
+    background-color: #e0c096;
+    border-bottom: 1px solid rgb(48, 49, 51);
+    border-right: 1px solid rgb(48, 49, 51);
+  }
 `;
 
 export const Info = ({ score, level, lines }: { score: number; level: number; lines: number }) => (
@@ -27,10 +55,12 @@ export const Info = ({ score, level, lines }: { score: number; level: number; li
       <Line>Score</Line>
       <Line>{score}</Line>
     </Group>
+    <Divider />
     <Group>
       <Line>Level</Line>
       <Line>{level}</Line>
     </Group>
+    <Divider />
     <Group>
       <Line>Lines</Line>
       <Line>{lines}</Line>
