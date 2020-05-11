1288e3b67f0551788fcc6f4280ba122e0b1acc29
GibboK <gibbok.coding@gmail.com>
2020-05-11 22:22:18 +0200

    add base logo

--- src/components/ScreenIntro.tsx
+++ src/components/ScreenIntro.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { gameSlice } from '~store';
 import { useDispatch } from 'react-redux';
+import { Logo } from '../assets/Logo';
+
 const {
   actions: { screenGame }
 } = gameSlice;
@@ -12,6 +14,7 @@ export const ScreenIntro = ({}: {}) => {
   const dispatch = useDispatch();
   return (
     <ScreenIntroStyled>
+      <Logo />
       <div onClick={() => dispatch(screenGame())}>INTRO - Click here to start</div>
     </ScreenIntroStyled>
   );
