b8b1f42d33b1d91906fe579ae1bdc0e29e2675db
GibboK <gibbok.coding@gmail.com>
2020-05-29 09:30:36 +0200

    Revert "add support for different viewports in storybook"
    This reverts commit ec4f4b8db144bfd4abf853dbddc4f1d7d637155b.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,8 @@
 // require('loki/configure-react')
 const { addParameters } = require('@storybook/react')
 const { create } = require('@storybook/theming/create')
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 addParameters({
-  viewport: {
-    viewports: {
-      ...INITIAL_VIEWPORTS,
-    },
-  },
   options: {
     theme: create({
       base: 'light',
