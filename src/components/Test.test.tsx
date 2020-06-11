ae8b878970f401cf32783dd12231695cd67e5e26
GibboK <gibbok.coding@gmail.com>
2020-06-11 17:30:31 +0200

    add tests

---
+++ src/components/Test.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer, { act } from 'react-test-renderer';
+import { Test } from './Test';
+
+describe('test', () => {
+  jest.useFakeTimers();
+  it('should xx', () => {
+    act(() => {
+      const tree = renderer.create(<Test />);
+      jest.advanceTimersByTime(5000);
+      expect(tree).toMatchSnapshot();
+
+      jest.advanceTimersByTime(10000);
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
