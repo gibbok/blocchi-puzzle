4a21055730757e58326c7ae87cdd088a06105aa4
GibboK <gibbok.coding@gmail.com>
2020-06-11 20:49:07 +0200

    add tests

---
+++ src/components/GameLoop.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { GameLoop } from './GameLoop';
+import { mockStore } from '../utils';
+import { Provider } from 'react-redux';
+
+describe('<GameLoop />', () => {
+  const dkr = {
+    get: jest.fn(),
+    set: jest.fn(),
+  };
+  const cb = jest.fn();
+
+  it('should not rendere any dome', () => {
+    const store = mockStore();
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <GameLoop level={1} detectionKeyRepeat={dkr} cb={cb} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
