4a5365f1774a660fd6cb3f4ca9f141f181ca56d9
GibboK <gibbok.coding@gmail.com>
2020-06-11 22:31:17 +0200

    CHANGE to GAMELOOP

--- src/components/GameLoop.test.tsx
+++ src/components/GameLoop.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { GameLoop } from './GameLoop';
+import { GameLoop, loop } from './GameLoop';
 import { mockStore } from '../utils';
 import { Provider } from 'react-redux';
 
@@ -10,9 +10,9 @@ describe('<GameLoop />', () => {
     set: jest.fn(),
   };
   const cb = jest.fn();
+  const store = mockStore();
 
   it('should not render any dom', () => {
-    const store = mockStore();
     const tree = renderer
       .create(
         <Provider store={store}>
@@ -21,5 +21,22 @@ describe('<GameLoop />', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  describe('loop', () => {
+    const dkr = {
+      get: jest.fn().mockImplementation(() => false),
+      set: jest.fn(),
+    };
+    const setLastTimeCbMock = jest.fn();
+    const cbMock = jest.fn();
+    const dispatchCbMock = jest.fn().mockImplementation(() => undefined);
+    it('should call dispatch and update last time, when user does not hold key', () => {
+      loop(5000, 1, 1000, dkr, setLastTimeCbMock, cbMock, dispatchCbMock);
+      expect(setLastTimeCbMock).toHaveBeenCalled();
+      expect(cbMock).not.toHaveBeenCalled();
+      expect(dispatchCbMock).toHaveBeenCalled();
+    });
   });
 });
--- src/components/GameLoop.tsx
+++ src/components/GameLoop.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Action } from '@reduxjs/toolkit';
 import { DetectorKeyRepeat } from './detectorKeyRepeat';
 
 const TICK_MS = 1000;
@@ -9,32 +8,51 @@ const calcTimeClockByLevel = (base: number, level: number) => base - level * (TI
 
 let animId = -1;
 
+export const loop = (
+  time: number,
+  level: number,
+  lastTime: number,
+  detectionKeyRepeat: DetectorKeyRepeat,
+  setLastTimeCb: (time: number) => void,
+  cb: () => void,
+  dispatchCb: (cb: () => void) => void
+): void => {
+  const isKeyHeld = detectionKeyRepeat.get();
+  const threshold = calcTimeClockByLevel(TICK_MS, level);
+  const progress = time - lastTime;
+  const canGameAdvance = progress >= threshold;
+
+  console.log('--------------');
+  console.log('isKeyHeld', isKeyHeld);
+  console.log('threshold', isKeyHeld);
+  console.log('progress', progress);
+  console.log('canGameAdvance', canGameAdvance);
+
+  if (canGameAdvance) {
+    setLastTimeCb(time);
+    if (!isKeyHeld) {
+      dispatchCb(cb);
+    }
+    return undefined;
+  }
+  animId = window.requestAnimationFrame((time) =>
+    loop(time, level, lastTime, detectionKeyRepeat, setLastTimeCb, cb, () => dispatchCb(() => cb()))
+  );
+  return undefined;
+};
+
 type Props = Readonly<{ level: number; detectionKeyRepeat: DetectorKeyRepeat; cb: () => void }>;
 
 export const GameLoop = ({ level, detectionKeyRepeat, cb }: Props): JSX.Element => {
   const dispatch = useDispatch();
   const [lastTime, setLastTime] = React.useState(0);
 
-  const loop = (time: number): void | Action => {
-    const isKeyHeld = detectionKeyRepeat.get();
-    const threshold = calcTimeClockByLevel(TICK_MS, level);
-    const progress = time - lastTime;
-    const canGameAdvance = progress >= threshold;
-    if (canGameAdvance) {
-      setLastTime(time);
-      if (!isKeyHeld) {
-        dispatch(cb());
-      }
-      return undefined;
-    }
-    animId = window.requestAnimationFrame(loop);
-    return undefined;
-  };
-
   const cleanAnimation = (id: number) => window.cancelAnimationFrame(id);
 
   useEffect(() => {
-    animId = window.requestAnimationFrame(loop);
+    animId = window.requestAnimationFrame((time) =>
+      loop(time, level, lastTime, detectionKeyRepeat, setLastTime, cb, () => dispatch(cb()))
+    );
     return () => cleanAnimation(animId);
   });
   return <></>;
