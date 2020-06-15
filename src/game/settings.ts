7abffe7a58dc749b860beba758f903add5fac1e7
GibboK <gibbok.coding@gmail.com>
2020-06-15 17:46:45 +0200

    resize buttons

--- src/game/settings.ts
+++ src/game/settings.ts
@@ -12,6 +12,7 @@ export enum PaletteEnum {
   Cedar = '#442217',
   Tan = '#d7b185',
   Paco = '#32180c',
+  White = '#ffffff',
 }
 export const TILE_COLOR_NOTETRO = PaletteEnum.Paco;
 export enum TITLE_COLOR_ENUM {
@@ -29,11 +30,13 @@ export const VP_SM = 600;
 export const VP_MD = 960;
 export const VP_LG = 1280;
 export const VP_XL = 1930;
-export type Size = 'sm' | 'md' | 'lg' | 'xl';
+export type Size = 'sm' | 'md' | 'lg' | 'xl' | 'md_l' | 'md_p';
 export type SizeMap = Record<Size, number>;
 export const SIZE: SizeMap = {
   sm: VP_SM,
   md: VP_MD,
+  md_l: VP_MD,
+  md_p: VP_MD,
   lg: VP_LG,
   xl: VP_XL,
 };
@@ -41,6 +44,8 @@ export const SIZE: SizeMap = {
 export const DEVICE: Record<Size, string> = {
   sm: `(min-width: 0px) and (max-width: ${VP_SM}px)`,
   md: `(min-width: ${VP_SM}px) and (max-width: ${VP_LG}px)`,
+  md_l: `(min-width: ${VP_SM}px) and (max-width: ${VP_LG}px) and (orientation:landscape)`,
+  md_p: `(min-width: ${VP_SM}px) and (max-width: ${VP_LG}px) and (orientation:portrait)`,
   lg: `(min-width: ${VP_LG}px) and (max-width: ${VP_XL}px)`,
   xl: `(min-width: ${VP_XL}px)`,
 };
@@ -48,6 +53,8 @@ export const DEVICE: Record<Size, string> = {
 export const mq: Record<Size, string> = {
   sm: `@media ${DEVICE.sm}`,
   md: `@media ${DEVICE.md}`,
+  md_l: `@media ${DEVICE.md_l}`,
+  md_p: `@media ${DEVICE.md_p}`,
   lg: `@media ${DEVICE.lg}`,
   xl: `@media ${DEVICE.xl}`,
 };
