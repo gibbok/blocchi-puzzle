8ae4d4953fac435634abfc15ddc9b31829044caf
GibboK <gibbok.coding@gmail.com>
2020-06-23 00:21:30 +0200

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -15,8 +15,8 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   coverageThreshold: {
     global: {
-      statements: 98,
-      branches: 94,
+      statements: 97,
+      branches: 95,
       lines: 98,
       functions: 94,
     },
