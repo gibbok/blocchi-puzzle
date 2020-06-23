3f7422a7cdf1ffced90131c12396871f5d619715
GibboK <gibbok.coding@gmail.com>
2020-06-23 09:30:48 +0200

    add tests

--- src/context.test.tsx
+++ src/context.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useAppContextConsumer, AppContextProvider } from './context';
 import { mockContext } from './utils';
 import renderer from 'react-test-renderer';
+import { act } from 'react-dom/test-utils';
+import ReactDOM from 'react-dom';
 
 function TestComp(): JSX.Element {
   const { repeat, browserInfo, setRepeat } = useAppContextConsumer();
@@ -11,11 +13,27 @@ function TestComp(): JSX.Element {
         repeat,
         browserInfo,
       })}
+      <button type="button" onClick={() => setRepeat(true)}>
+        Click me!
+      </button>
     </div>
   );
 }
 
 describe('context', () => {
+  let container: Element | null;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    if (container) {
+      document.body.removeChild(container);
+      container = null;
+    }
+  });
   it('should use AppContextProvider', () => {
     const context = mockContext();
     const tree = renderer
@@ -39,5 +57,21 @@ describe('context', () => {
     }
 
     expect(exception.message).toBe('useContext must be inside a Provider with a value');
+  });
+
+  it('should call setRepeat', () => {
+    act(() => {
+      const context = mockContext();
+      ReactDOM.render(
+        <AppContextProvider value={context}>
+          <TestComp />
+        </AppContextProvider>,
+        container
+      );
+      container?.querySelector('button')?.click();
+      const hasRepeatChanged = container?.innerHTML.search('true');
+
+      expect(hasRepeatChanged).toBeTruthy();
+    });
   });
 });
